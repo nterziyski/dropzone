dd7cc001e101f94742e0aec9d2bae0f5214b217c
Nikolay Terziyski <nikolai.terziiski@gmail.com>
2019-11-10 21:41:59 +0100

    Updates

---
+++ precache-manifest.9f966be1c8a8c293ed0b5a953ec98b4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1064b902dabaceeb9aa5d7adcb485eeb",
+    "url": "/dropzone/index.html"
+  },
+  {
+    "revision": "55b077e9f894b107708f",
+    "url": "/dropzone/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "161edc225098279836b3",
+    "url": "/dropzone/static/js/2.60bf3c99.chunk.js"
+  },
+  {
+    "revision": "55b077e9f894b107708f",
+    "url": "/dropzone/static/js/main.ae7bcd7d.chunk.js"
+  },
+  {
+    "revision": "6cfa0c6ea71953276ebb",
+    "url": "/dropzone/static/js/runtime-main.72804842.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/dropzone/static/media/logo.25bf045c.svg"
+  }
+]);
